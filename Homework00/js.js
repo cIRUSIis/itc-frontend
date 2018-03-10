abb9713ff7dc54246e156cf6a875340c31e79211
cIRUSIis <nikitaspy1975@gmail.com>
2018-03-10 23:09:55 +0300

    Design corrections

---
+++ Homework00/js.js
@@ -0,0 +1,65 @@
+// Б.З. №1
+function plus(a, b) {
+  return a + b
+}
+console.log('Б.З. 1 = ' + plus(1, 2))
+
+// Б.З. №2
+function multiply(a, b) {
+  return a * b
+}
+console.log('Б.З. 2 = ' + multiply(3, 10)) // Зачем здесь plus и for?
+
+// Б.З. №3
+function mergeArrays(arr1, arr2) {
+  arr1.push(arr2)
+  return arr1
+}
+console.log('Б.З. 3 = ' + mergeArrays([1, 2, 3], [4, 5]))
+
+//Б.З. №4 - не совсем понял, что нужно, но как то так
+function filterFemales(people) {
+  for (var i = 0; i < humanCount; i++) {
+    if (people[i].sex == 'F') {
+      console.log(people[i].name + ' любит ' + people[i].film)
+    }
+  }
+}
+
+let people = []
+let human = {}
+let humanCount = prompt('Введи кол-во людей', '')
+for (var i = 0; i < humanCount; i++) {
+  people[i] = {
+    name: prompt('Введи имя', ''),
+    sex: prompt('Введи пол (M/F)', ''),
+    film: prompt('Введи любимый фильм', '')
+  }
+}
+filterFemales(people)
+
+//Б.З. №5
+function getQuadrant(x, y) {
+  if (x == 0) {
+    if (y == 0) {
+      console.log('Точка находится в начале координат')
+    } else {
+      console.log('Точка лежит на оси OY')
+    }
+  } else if (y == 0) {
+    console.log('Точка лежит на оси OX')
+  } else if (x > 0 && y > 0) {
+    console.log('Точка лежит в I коорд. четверти')
+  } else if (x < 0 && y > 0) {
+    console.log('Точка лежит во II коорд. четверти')
+  } else if (x < 0 && y < 0) {
+    console.log('Точка лежит в III коорд. четверти')
+  } else {
+    console.log('Точка лежит в IV коорд. четверти')
+  }
+}
+getQuadrant(1, 2)
+getQuadrant(-1, 2)
+getQuadrant(-2, -3)
+getQuadrant(4, -7)
+getQuadrant(0, 0)
